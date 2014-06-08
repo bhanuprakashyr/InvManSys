8dfd7bd4c8f3e67626f3601d61b886db164b38f9
bhanuprakash <prakashbhanu57@gmail.com>
2014-06-08 14:02:35 +0530

    my lovely git steps

---
+++ src/test/spring/ConfigureAndCommit.java
@@ -0,0 +1,11 @@
+package test.spring;
+
+public class ConfigureAndCommit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("coomit locally");
+
+	}
+
+}
